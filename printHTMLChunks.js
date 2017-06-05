1cb44bb2d5da5b4e79f3a6b30b4242962ebc5402
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-05 18:45:40 +0000

    created getAndPrintHTMLChunks function to print response data

---
+++ printHTMLChunks.js
@@ -0,0 +1,28 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+function getAndPrintHTMLChunks () {
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      console.log('Chunk:', data + '/n');
+    });
+
+  });
+}
+
+
+
+getAndPrintHTMLChunks();
